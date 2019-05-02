5b587d041e66ff33055051a1c0557ba122724b7c
madhuri <devarakonda.laxmi@gmail.com>
2019-05-02 01:16:47 -0500

    java script for footer

---
+++ web/footer.js
@@ -0,0 +1,42 @@
+ $(document).ready(function(){
+    $(".map").hover(function(){
+            $(this).fadeToggle();
+          },
+          function(){
+             $(this).fadeToggle();
+                });
+    $(".adlogin").hover(function(){
+            $(this).fadeToggle();
+          },
+          function(){
+                    $(this).fadeToggle();
+                });
+    $("ul li:first").hover(function(){
+            $(this).fadeToggle();
+          },
+          function(){
+                    $(this).fadeToggle();
+                });            
+                
+    $("ul li:second").hover(function(){
+            $(this).fadeToggle();
+          },
+          function(){
+                    $(this).fadeToggle();
+                });      
+                
+    $("ul li:third").hover(function(){
+            $(this).fadeToggle();
+          },
+          function(){
+                    $(this).fadeToggle();
+                });                  
+                
+                
+                
+                
+     });
+     
+    
+
+
